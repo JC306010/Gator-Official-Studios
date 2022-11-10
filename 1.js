c8fc8c2280fcedc74e3fb979a9730a5187abc093
JC306010 <116584138+JC306010@users.noreply.github.com>
2022-11-09 23:39:38 -0600

    Changed Headings

---
+++ 1.js
@@ -0,0 +1,13 @@
+console.log('Hello World!')
+let btn = document.createElement("button")
+appendChild <a href="#" class="font-semibold text-gray-900 hover:text-gray-900">Gator Music Blog</a>
+
+// Function to create a button
+function createButton() {
+    let btn = document.createElement("button")
+    btn.innerHTML = "Click Me"
+    btn.onclick = () => {
+        alert("Hello World!")
+    }
+    document.body.appendChild(btn)
+}
